45469d0405dbf125cac4268c74e27f3a8952ae7c
roYCalderon <royedc4@gmail.com>
2015-11-12 01:07:47 -0430

    newLanding

---
+++ server/ajax/Compressors/recInfo.php
@@ -0,0 +1,14 @@
+<?php 
+require_once '../../secure/db.php'; // The mysql database connection script
+
+$outerQuery="CALL `compressor-recInfo`()";
+
+$outerResult=$mysqli->query($outerQuery) or die($mysqli->error.__LINE__);
+$arr = array();
+if($outerResult->num_rows > 0) {
+	while($row = $outerResult->fetch_assoc()) {
+		$arr[] = $row;
+	}
+}
+echo $json_response = json_encode($arr);
+?>
